93557adff6f1af7b654a9ec0684bc4ab3e354992
Kelly Mello <2610433@unicesusc.edu.br>
2026-09-23 15:42:28 -0300

    Venha conhecer o mais novo transporte de Pets personalizado. Transportando seu pet com segurança. Hoje transportamos 3 cachorros e 5 gatos.

---
+++ petsitter.py
@@ -0,0 +1,5 @@
+print('Venha conhecer o mais novo transporte de Pets personalizado. Transportando seu pet com segurança.')
+cachorro=int(input('Escreva um número: '))
+gato=int(input('Escreva outro número: '))
+total=gato+cachorro
+print(f'Hoje transportamos {cachorro} cachorros e {gato} gatos. ')
